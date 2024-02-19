7029db5e76261d4b1b99976b02d22918d1b61341
Giovanni Puntil <giovanni.puntil@matchesfashion.com>
2024-02-19 23:05:51 +0000

    Update ref

--- components/Card/Title.js
+++ components/Card/Title.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function CardTitle(props) {
   return <>
     <div class="media is-align-items-center">
-      {props.icon && <div class="media-left mr-3"><Image src={`images/${props.icon}.svg`} width={24} height={24} /></div>}
+      {props.icon && <div class="media-left mr-3"><Image src={`/giovannipuntil/images/${props.icon}.svg`} width={24} height={24} /></div>}
       <div class="media-content">
         <div className="is-size-6 is-size-7-touch has-text-weight-semibold has-text-black is-uppercase">{props.children}</div>
       </div>
--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="theme-color" content="#ffffff" />
-        <script src="/static/theme.js" />
+        <script src="/giovannipuntil/static/theme.js" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <title>Giovanni Puntil – software engineer, designer, maker</title>
         <meta name="description" content="Giovanni Puntil – software engineer, designer, maker. I find joy in the process of creation, whether physical or virtual." />
-        <link rel="stylesheet" href="/static/bulma.css"></link>
+        <link rel="stylesheet" href="/giovannipuntil/static/bulma.css"></link>
       </Head>
       <body>
         <Main />

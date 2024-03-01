632a8ca6e27a6a9f64df3fc6dba079813711c357
Giovanni Puntil <giovanni.puntil@matchesfashion.com>
2024-03-01 14:25:52 +0000

    move title, description. viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import clsx from 'clsx';
 import { Inter_Tight, Inter } from 'next/font/google'
 
@@ -18,6 +19,11 @@ const inter = Inter({
 export default function App({ Component, pageProps }) {
   return (
     <div className={clsx(inter.variable, interTight.variable)}>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <title>Giovanni Puntil – software engineer, designer, maker</title>
+        <meta name="description" content="Giovanni Puntil – software engineer, designer, maker. I find joy in the process of creation, whether physical or virtual." />
+      </Head>
       <Component {...pageProps} />
     </div>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,6 @@ export default function Document() {
       <Head>
         <meta name="theme-color" content="#ffffff" />
         <script src="/static/theme.js" />
-        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <title>Giovanni Puntil – software engineer, designer, maker</title>
-        <meta name="description" content="Giovanni Puntil – software engineer, designer, maker. I find joy in the process of creation, whether physical or virtual." />
         <link rel="stylesheet" href="/static/bulma.css"></link>
       </Head>
       <body>
